06f072fb5aab0ed620d66a855c49e7b09ed04fdd
Ana Paula Bohnenberger <anapaulabohnenberger@gmail.com>
2023-07-17 19:10:55 -0300

    conectando no banco de dados

--- devagram-node-nextjs/pages/api/login.ts
+++ devagram-node-nextjs/pages/api/login.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
 
-export default (
+const endpointLogin = (
     req : NextApiRequest,
     res : NextApiResponse
 ) => {
@@ -9,9 +10,11 @@ export default (
 
         if(login === 'admin@admin.com' &&
             senha === 'Admin@123'){
-                res.status(200).json({msg : 'Usuário autentificado com sucesso'});
+                return res.status(200).json({msg : 'Usuário autentificado com sucesso'});
         }
         return res.status(400).json({erro: 'Usuario ou senha nao encontrados'});
     }
     return res.status(405).json({erro: 'Metodo informado nao e valido'});
 }
+
+export default conectarMongoDB(endpointLogin);
