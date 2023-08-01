c5360b77f89cf8cba7c978836ff10429e42a0c42
Ana Paula Bohnenberger <anapaulabohnenberger@gmail.com>
2023-08-01 10:53:16 -0300

    jwt autentificacao

--- devagram-node-nextjs/pages/api/cadastro.ts
+++ devagram-node-nextjs/pages/api/cadastro.ts
@@ -1,9 +1,12 @@
 import {NextApiRequest, NextApiResponse} from 'next';
 import {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 import {CadastroRequisicao} from '../../types/CadastroRequisicao';
+import {UsuarioModel} from '../../models/UsuarioModel';
+import {conectarMongoDB} from '../../middlewares/conectarMongoDB'
+import md5 from 'md5';
 
 const endpointCadastro = 
-    (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
+    async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
 
     if(req.method === 'POST'){
         const usuario = req.body as CadastroRequisicao;
@@ -22,9 +25,22 @@ const endpointCadastro =
             return res.status(400).json({erro : 'Senha invalida'});
         }
 
-        return res.status(200).json({erro : 'Dados corretos'});
+        // validacao se ja existe usuario com o mesmo email
+        const usuariosComMesmoEmail = await UsuarioModel.find({email : usuario.email});
+        if(usuariosComMesmoEmail && usuariosComMesmoEmail.length > 0){
+            return res.status(400).json({erro : 'Ja existe uma conta com o email informado'});
+        }
+
+        // salvar no banco de dados dados
+        const usuarioASerSalvo = {
+            nome : usuario.nome,
+            email : usuario.email,
+            senha : md5(usuario.senha), 
+        }
+        await UsuarioModel.create(usuarioASerSalvo);
+        return res.status(200).json({erro : 'Usuario cadastrado com sucesso'});
     }
     return res.status(405).json({erro : 'Metodo informado nao e valido'});
 }
 
-export default endpointCadastro;
+export default conectarMongoDB(endpointCadastro);
